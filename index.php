f8573554dc4bc92f2c39e716cf3fadb022e0fb47
anshul <ansh14j@gmail.com>
2018-10-19 15:12:40 +0530

    added font

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">	
 <link rel="stylesheet" type="text/css" href="mystyle.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
 </head>
-<body>
+<body style="font-family: 'Montserrat', sans-serif;">
 
 	<div>
 		<div class="container-fluid">
--- keep.php
+++ keep.php
@@ -13,6 +13,8 @@ session_start();
 <head>
 	<title>myKeep</title>
 	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
 
 	<script>
 	function updateCheckBox(domItem,list,item) {
@@ -118,7 +120,6 @@ session_start();
 	}
 	function enterPressed(event,domList,l){
 		var x = event.key;
-		//var i = document.getElementById(domList).value;
 			
 		if(x==="Enter"){
 			addItem(domList,l);
@@ -127,9 +128,7 @@ session_start();
 	function makeUL(domId,lName,lId) {
 		// Create the list item:
 		let item = document.createElement('ul');
-		//item.id = "list_".concat(lName);
 		item.id = "list_".concat(lId).concat(lName)
-		item.style="list-style-type:none";
 		// Set its contents:
 		item.appendChild(document.createTextNode(lName));
 		// Add it to the list:
@@ -142,35 +141,24 @@ session_start();
 	        // Create the list item:
 	        let item = document.createElement('li');
 			let itemText = document.createElement('span');
-			//item.id = "item_".concat(listName).concat(arrayitem);
 			
 	        // Set its contents:
 	        itemText.appendChild(document.createTextNode(arrayitem));
 			item.appendChild(itemText);
 
 	        // Add it to the list:
-			// console.log(domList);
 			let domItem = domList.appendChild(item);
 			makeDelete(domItem,listName,arrayitem);
 			makeCheckBox(domItem, arrayitem, isChecked, listName);
 
-			// console.log(document.getElementById('item_'.concat(listName).concat(arrayitem)).childNodes[0]);
 			domItem.childNodes[0].onclick=function(){makeChangeTextBox(domItem,arrayitem,listName);}
-
-	    // Finally, return the constructed list:
-	    //return list;
 	}
 	function makeChangeTextBox(domItem,arrayitem,listName){
 		var domArrayItem = domItem.childNodes[0].innerText;
-		// console.log(domArrayItem);
 		var newItem = document.createElement("input");
-		//console.log('item_'.concat(listName).concat(arrayitem));
 		newItem.value = domArrayItem;
-		newItem.style = 'width:100%; border:none; border-bottom:1px solid lightgrey; border-top:1px solid lightgrey;outline: none;padding:4px 20px;font:70% helvetica;';
 		newItem.id = "new";
 		
-		//newItem.focus();
-
 		var list = domItem.parentNode;
 		list.insertBefore(newItem, domItem);
 		domItem.previousSibling.focus();
@@ -188,10 +176,8 @@ session_start();
 		var x = event.key;
 			
 		if(x==="Enter"){
-			//domItem.parentNode.lastChild.focus();
 			if(domItem.nextSibling !==domItem.parentNode.lastChild)
 				makeChangeTextBox(domItem.nextSibling,domItem.nextSibling.childNodes[0].innerText,listName);
-				//makeli(domItem.parentNode,"",domItem.parentNode.childNodes[0],0)
 			else
 				domItem.parentNode.lastChild.focus();
 		}
@@ -202,8 +188,6 @@ session_start();
 		xmlhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				domItem.childNodes[0].innerText=i;				///why not innerHTML
-				//document.getElementById('item_'.concat(listName).concat(arrayitem)).id='item_'.concat(listName).concat(i);
-				//domItem.style='display:block';
 				let checkBox = domItem.childNodes[2];
 				if (checkBox.checked == true)
 					domItem.style='text-decoration:line-through;opacity: .5;display:block';
@@ -219,16 +203,13 @@ session_start();
 		xmlhttp.send();	
 	}
 	function makeCheckBox(domItem, arrayitem, isChecked,listName) {
-	    // Create the list element:
 	        // Create the list item:
 	        var item = document.createElement('input');	
-			//	item.id = "item_input_".concat(listName).concat(arrayitem);
 			item.class= "isChecked";
 			item.type="checkbox";
 			if(isChecked==1){
 				item.checked = true;
 				domItem.style='text-decoration:line-through;opacity: .5';
-				//console.log("0");
 			}
 			else
 				item.checked= false;
@@ -238,10 +219,8 @@ session_start();
 	        domItem.appendChild(item);
 	}
 	function makeDelete(domItem,listName,itemName) {
-	    // Create the list element:
 	        // Create the list item:
 	        var item = document.createElement('input');
-			//item.id = "item_del_".concat(listName).concat(itemName); // not necessary , but need improvemnt in code to remove this
 			item.class= "del";
 			item.type="button";
 			item.value="x";
@@ -251,10 +230,8 @@ session_start();
 	        domItem.appendChild(item);
 	}	
 	function makeDeleteList(domList,listName) {
-	    // Create the list element:
 	        // Create the list item:
 	        var item = document.createElement('input');
-			//item.id = "list_".concat(listName);
 			item.classList= "del2";
 			item.type="button";
 			item.value="Delete List";
@@ -262,11 +239,8 @@ session_start();
 																				
 	        // Add it to the list:
 	        domList.appendChild(item);
-	    // Finally, return the constructed list:
-	    //return list;
 	}	
 	function makeInputText(domList,listName) {
-	    // Create the list element:	    
 	        // Create the list item:
 	        var item = document.createElement('input');
 			item.id = "input_".concat(listName);
@@ -275,8 +249,6 @@ session_start();
 			item.onkeydown=function(){enterPressed(event, domList, listName);}											
 	        // Add it to the list:
 	        domList.appendChild(item);
-	    // Finally, return the constructed list:
-	    //return list;
 	}
 	function getlist(){
 		xmlhttp = new XMLHttpRequest();
@@ -286,7 +258,6 @@ session_start();
 				let i = 0;		
 				while(myObj[i])
 				{
-					//console.log(myObj[i].listName);
 					let domList = makeUL(document.getElementById('demo21'), myObj[i].listName, myObj[i].listID);
 					getItems(domList, myObj[i].listName);
 					i++;
@@ -318,7 +289,8 @@ session_start();
 	}
 	</script>
 </head>
-<body>
+<body style="font-family: 'Montserrat', sans-serif;">
+
 
 <div id="demo21"></div>
 
